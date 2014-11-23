ecbc47551addc37ad03f1dd925fa0403c8559191
PlasmaSheep <plasmasheep@gmail.com>
2014-11-23 12:34:22 -0800

    Wrote basic scraping to get course titles, group titles, and subgroup titles

--- boethius/scrape.py
+++ boethius/scrape.py
@@ -5,6 +5,25 @@
 import models
 import yaml
 import pdb
+import urllib
+from bs4 import BeautifulSoup
+
+def scrape(url):
+    """Scrape the given url for classes.
+    """
+    page = urllib.urlopen(url)
+    soup = BeautifulSoup(page)
+
+    group = soup.find_all("tr", class_="SAHeaderYellowBar")[0].get_text().strip()
+    print "Found group: " + group
+
+    subgroup = soup.find_all("tr", class_="SAHeaderGreenBar")[0].get_text().strip()
+    print " Found subgroup: " + subgroup
+
+    classes = soup.find_all("tr", class_="SAHeaderDarkGreenBar")
+
+    for class_element in classes:
+        print "  Found class: " + class_element.get_text().strip()
 
 def main():
     with open("config.yaml") as f:
@@ -12,7 +31,7 @@ def main():
 
     #pdb.set_trace()
     for source in config["sources"]:
-        print source
+        scrape(source)
 
 if __name__ == "__main__":
     main()
